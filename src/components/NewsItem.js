b0b0a9ec3f9f576341d6b68a596121f17b421a22
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-01 15:17:13 +0530

    link bootstape css and javascript

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+    constructor() {
+        super();
+        console.log("Hello i am constructor")
+    }
     render() {
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
-            <div>
-                This is a news item
-                
+            <div className = "my-3">
+                <div className="card" style={{ width: "18rem"}}>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title} ...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} taget ="_blank" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
+                </div>
+
             </div>
         )
     }
