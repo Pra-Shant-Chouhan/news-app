26e0ad3479d370409e986215207717bab98728ab
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-01 19:19:00 +0530

    Navbar color changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-warning bg-warning">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/">NewsChunky</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
