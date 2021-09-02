e40abbdb8dce4fdb4d31627d8271b7b7a6cb7362
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-02 18:20:21 +0530

    passing cetegory props

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <Navbar />
-        <News pageSize={9} country = "in"/>
+        <News pageSize={9} country = "in" category = "technology" />
         
       </div>
     )
