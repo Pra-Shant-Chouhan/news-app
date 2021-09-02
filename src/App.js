67d87f44e4a01a68d6ecd2091fa44503b9ca7e84
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-02 16:52:22 +0530

    passing props in news component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <Navbar />
-        <News/>
+        <News pageSize={9} country = "in"/>
         
       </div>
     )
