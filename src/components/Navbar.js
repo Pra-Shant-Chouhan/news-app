372c2c553cdb24296ef91261a744cc02499526cb
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-02 18:19:51 +0530

    adding Navbar component like science, sports

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,12 +20,18 @@ export class Navbar extends Component {
                                 <li className="nav-item">
                                     <a className="nav-link active" aria-current="page" href="/">Home</a>
                                 </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/">About</a>
-                                </li>
-                                
+                                <li className="nav-item"><a className="nav-link" href="/">About</a> </li>
+                                <li className="nav-item"><a className="nav-link" href="/business">Business</a> </li>
+                                <li className="nav-item"><a className="nav-link" href="/entertainment">Entertainment</a> </li>
+                                <li className="nav-item"><a className="nav-link" href="/general">General</a> </li>
+                                <li className="nav-item"><a className="nav-link" href="/health">Health</a> </li>
+                                <li className="nav-item"><a className="nav-link" href="/science">Science</a> </li>
+                                <li className="nav-item"><a className="nav-link" href="/sports">Sports</a> </li> 
+                                <li className="nav-item"><a className="nav-link" href="/technology">Technology</a> </li>
+
+
                             </ul>
-                            
+
                         </div>
                     </div>
                 </nav>
