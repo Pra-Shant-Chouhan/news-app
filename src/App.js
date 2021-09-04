0d68dcd23b6235aa17d5a713965d1fbb4a2bd5da
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-09-04 20:39:07 +0530

    sports link not loding problem slove & pageSize set

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 
 
 export default class App extends Component {
-  pageSize = 15;
+  pageSize = 6;
   render() {
     return (
       <div>
@@ -19,14 +19,14 @@ export default class App extends Component {
 
           <Navbar />
           <Switch>
-            <Route exact path="/"> <News key="general"  pageSize={this.pageSize} country="in" category="general"/></Route>
-            <Route exact path="/business"> <News key="business" pageSize={this.pageSize} country="in" category="business"/></Route>
-            <Route exact path="/entertainment"> <News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/></Route>
-            <Route exact path="/general"> <News key="general" pageSize={this.pageSize} country="in" category="general"/></Route>
-            <Route exact path="/health"> <News key="health" pageSize={this.pageSize} country="in" category="health"/></Route>
-            <Route exact path="/science"> <News key="science" pageSize={this.pageSize} country="in" category="science"/></Route>
-            <Route exact path="/sports "> <News key="sports" pageSize={this.pageSize} country="in" category="sports"/></Route>
-            <Route exact path="/technology"> <News key="technology" pageSize={this.pageSize} country="in" category="technology"/></Route>
+            <Route exact  path="/"> <News key="general"  pageSize={this.pageSize} country="in" category="general"/></Route>
+            <Route exact  path="/business"> <News key="business" pageSize={this.pageSize} country="in" category="business"/></Route>
+            <Route exact  path="/entertainment"> <News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/></Route>
+            <Route exact  path="/general"> <News key="general" pageSize={this.pageSize} country="in" category="general"/></Route>
+            <Route exact  path="/health"> <News key="health" pageSize={this.pageSize} country="in" category="health"/></Route>
+            <Route exact  path="/science"> <News key="science" pageSize={this.pageSize} country="in" category="science"/></Route>
+            <Route exact  path="/sports"> <News key="sports" pageSize={this.pageSize} country="in" category="sports"/></Route>
+            <Route exact  path="/technology"> <News key="technology" pageSize={this.pageSize} country="in" category="technology"/></Route>
 
           </Switch>
         </Router>
